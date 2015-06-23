21d7e6dc9327c74db828a0cce85cc24d16d852a1
xubin <nybux.tsui@gmail.com>
2015-06-23 14:47:02 +0800

    添加配置文件

--- main.go
+++ main.go
@@ -2,16 +2,47 @@ package main
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/BurntSushi/toml"
 )
 
+var Config struct {
+	Listen  string `toml:"listen"`
+	Channel []struct {
+		Name    string   `toml:"name"`
+		Type    string   `toml:"type"`
+		Addr    string   `toml:"addr"`
+		Domains []string `toml:"domains"`
+	} `toml:"channel"`
+	Default struct {
+		Type string `toml:"type"`
+		Addr string `toml:"addr"`
+	} `toml:"default"`
+}
+
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:8080")
+	Config.Listen = "127.0.0.1:8080"
+	_, err := toml.DecodeFile("goproxy.conf", &Config)
+	if err != nil {
+		log.Fatalln("DecodeFile failed:", err)
+	}
+	for i, channel := range Config.Channel {
+		for j, d := range channel.Domains {
+			if !strings.HasPrefix(d, ".") {
+				Config.Channel[i].Domains[j] = "." + d
+			}
+		}
+	}
+
+	l, err := net.Listen("tcp", Config.Listen)
 	if err != nil {
 		log.Fatalln("Listen failed:", err)
 	}
@@ -23,6 +54,91 @@ func main() {
 		}
 		go doProxy(c)
 	}
+}
+
+func indomains(domain string, port uint16) (net.Conn, error) {
+	for _, channel := range Config.Channel {
+		for _, d := range channel.Domains {
+			if d[1:] == domain || strings.HasSuffix(domain, d) {
+				log.Println(channel.Name, ":", domain)
+				if channel.Type == "socks5" {
+					return connectSocks5(channel.Addr, domain, port)
+				} else {
+					return net.Dial("tcp", fmt.Sprintf("%s:%v", domain, port))
+				}
+			}
+		}
+	}
+	log.Println("default :", domain)
+	if Config.Default.Type == "socks5" {
+		return connectSocks5(Config.Default.Addr, domain, port)
+	} else {
+		return net.Dial("tcp", fmt.Sprintf("%s:%v", domain, port))
+	}
+}
+
+func connectSocks5(socks, domain string, port uint16) (net.Conn, error) {
+	c2, err := net.Dial("tcp", socks)
+	if err != nil {
+		log.Println("Conn.Dial failed:", err, socks)
+		return nil, err
+	}
+	c2.SetDeadline(time.Now().Add(10 * time.Second))
+	c2.Write([]byte{5, 1, 0})
+	resp := make([]byte, 2)
+	n, err := c2.Read(resp)
+	if err != nil {
+		log.Println("Conn.Read failed:", err)
+		return nil, err
+	}
+	if n != 2 {
+		log.Println("socks5 response error:", resp)
+		return nil, errors.New("socks5_error")
+	}
+	if resp[1] != 0 {
+		log.Println("socks5 not support 'NO AUTHENTICATION REQUIRED'")
+		return nil, errors.New("socks5_error")
+	}
+	send := make([]byte, 0, 512)
+	send = append(send, []byte{5, 1, 0, 3, byte(len(domain))}...)
+	send = append(send, []byte(domain)...)
+	send = append(send, byte(port>>8))
+	send = append(send, byte(port&0xff))
+	_, err = c2.Write(send)
+	if err != nil {
+		log.Println("Conn.Write failed:", err)
+		return nil, err
+	}
+	n, err = c2.Read(send[0:10])
+	if err != nil {
+		log.Println("Conn.Read failed:", err)
+		return nil, err
+	}
+	if send[1] != 0 {
+		switch send[1] {
+		case 1:
+			log.Println("socks5 general SOCKS server failure")
+		case 2:
+			log.Println("socks5 connection not allowed by ruleset")
+		case 3:
+			log.Println("socks5 Network unreachable")
+		case 4:
+			log.Println("socks5 Host unreachable")
+		case 5:
+			log.Println("socks5 Connection refused")
+		case 6:
+			log.Println("socks5 TTL expired")
+		case 7:
+			log.Println("socks5 Command not supported")
+		case 8:
+			log.Println("socks5 Address type not supported")
+		default:
+			log.Println("socks5 Unknown eerror:", send[1])
+		}
+		return nil, errors.New("socks5_error")
+	}
+	c2.SetDeadline(time.Time{})
+	return c2, nil
 }
 
 func doProxy(c net.Conn) {
@@ -49,74 +165,24 @@ func doProxy(c net.Conn) {
 			log.Println("http proxy format error, ':' not found")
 			return
 		}
-		domain := buff[8 : 8+p2]
+		domain := string(buff[8 : 8+p2])
 		p3 := bytes.IndexByte(buff[8+p2+1:n], ' ')
 		if p3 == -1 {
 			log.Println("http proxy format error, ' ' not found")
 			return
 		}
-		_port := buff[8+p2+1 : 8+p2+1+p3]
-		c2, err = net.Dial("tcp", "127.0.0.1:11081")
+		_port := string(buff[8+p2+1 : 8+p2+1+p3])
+		port, err := strconv.Atoi(_port)
 		if err != nil {
-			log.Println("Conn.Dial failed:", err)
+			log.Println("http proxy port format error, ", err)
 			return
 		}
-		c2.SetDeadline(time.Now().Add(10 * time.Second))
-		c2.Write([]byte{5, 1, 0})
-		resp := make([]byte, 2)
-		n, err := c2.Read(resp)
+		c2, err = indomains(domain, uint16(port))
 		if err != nil {
-			log.Println("Conn.Read failed:", err)
+			log.Println("connect socks5 failed:", err)
 			return
 		}
-		if n != 2 {
-			log.Println("socks5 response error:", resp)
-			return
-		}
-		if resp[1] != 0 {
-			log.Println("socks5 not support 'NO AUTHENTICATION REQUIRED'")
-			return
-		}
-		send := make([]byte, 0, 512)
-		send = append(send, []byte{5, 1, 0, 3, byte(len(domain))}...)
-		send = append(send, []byte(domain)...)
-		port, err := strconv.Atoi(string(_port))
-		send = append(send, byte(port>>8))
-		send = append(send, byte(port&0xff))
-		_, err = c2.Write(send)
-		if err != nil {
-			log.Println("Conn.Write failed:", err)
-			return
-		}
-		n, err = c2.Read(send[0:10])
-		if err != nil {
-			log.Println("Conn.Read failed:", err)
-			return
-		}
-		if send[1] != 0 {
-			switch send[1] {
-			case 1:
-				log.Println("socks5 general SOCKS server failure")
-			case 2:
-				log.Println("socks5 connection not allowed by ruleset")
-			case 3:
-				log.Println("socks5 Network unreachable")
-			case 4:
-				log.Println("socks5 Host unreachable")
-			case 5:
-				log.Println("socks5 Connection refused")
-			case 6:
-				log.Println("socks5 TTL expired")
-			case 7:
-				log.Println("socks5 Command not supported")
-			case 8:
-				log.Println("socks5 Address type not supported")
-			default:
-				log.Println("socks5 Unknown eerror:", send[1])
-			}
-			return
-		}
-		c2.SetDeadline(time.Time{})
+		defer c2.Close()
 		c.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n"))
 	} else {
 		p1 := bytes.Index(buff[:n], []byte("\r\nHost: "))
@@ -131,13 +197,23 @@ func doProxy(c net.Conn) {
 		}
 		url := string(buff[p1+8 : p1+8+p2])
 		p3 := strings.IndexByte(url, ':')
+		port := 80
+		_port := "80"
+		domain := url
 		if p3 == -1 {
 			url += ":80"
+		} else {
+			domain = url[:p3]
+			_port = string(url[p3+1:])
+			port, err = strconv.Atoi(_port)
+			if err != nil {
+				log.Println("http port format error:", _port)
+				return
+			}
 		}
-
-		c2, err = net.Dial("tcp", url)
+		c2, err = indomains(domain, uint16(port))
 		if err != nil {
-			log.Println("Conn.Dial failed:", err)
+			log.Println("connect socks5 failed:", err)
 			return
 		}
 		defer c2.Close()
@@ -152,7 +228,6 @@ func doProxy(c net.Conn) {
 
 	_, err = io.Copy(c2, c)
 	if err != nil {
-		log.Println("io.Copy failed:", err)
 		return
 	}
 }
@@ -163,7 +238,6 @@ func doPeer(c1 net.Conn, c2 net.Conn) {
 
 	_, err := io.Copy(c1, c2)
 	if err != nil {
-		log.Println("io.Copy failed:", err)
 		return
 	}
 }
